802059c4cb3cc331dac36804feef96675b6dd087
Kavilakshan12 <kavilakshanvijayakumar@gmail.com>
2025-04-25 14:50:44 +0530

    profileitem structured

--- client/src/components/profile/ProfileItem.js
+++ client/src/components/profile/ProfileItem.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// ProfileItem component displays individual user profile info
-const ProfileItem = ({
-  profile: {
-    user: { _id, name }, // Destructuring user id and name from the profile prop
-    location,            // Destructuring location from the profile prop
-  },
-}) => {
+/**
+ * ProfileItem Component
+ * Displays basic user profile information with a link to view more details.
+ *
+ * Props:
+ * - profile: {
+ *     user: { _id, name },
+ *     location
+ *   }
+ */
+const ProfileItem = ({ profile }) => {
+  const {
+    user: { _id, name },
+    location,
+  } = profile;
+
   return (
     <div className="profile bg-light">
       <div>
-        <h2>{name}</h2> {/* Displaying user name */}
+        <h2>{name}</h2>
         <p className="my-1">
-          {location && <span>{location}</span>} {/* Conditionally displaying location if it exists */}
+          {location && <span>{location}</span>}
         </p>
         <Link to={`/profile/${_id}`} className="btn btn-primary">
-          View User Profile {/* Link to the user's detailed profile page */}
+          View User Profile
         </Link>
       </div>
     </div>
   );
 };
 
-export default ProfileItem; // Exporting the component for use in other parts of the app
+export default ProfileItem;
