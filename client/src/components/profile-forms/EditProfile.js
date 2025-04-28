f046fbae6670c1e6d165c7d1f52d6f0b0464c024
Kavilakshan12 <kavilakshanvijayakumar@gmail.com>
2025-04-28 22:49:51 +0530

    design changed

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -3,7 +3,7 @@ import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createProfile, getCurrentProfile } from "../../actions/profile";
-import "./EditProfile.css"; // Make sure this path is correct
+import "./EditProfile.css";
 
 const EditProfile = ({
   createProfile,
@@ -34,7 +34,7 @@ const EditProfile = ({
       facebook: loading || !profile.facebook ? "" : profile.facebook,
       instagram: loading || !profile.instagram ? "" : profile.instagram
     });
-  }, [loading, getCurrentProfile]); // Note the dependencies added
+  }, [loading, getCurrentProfile]);
 
   const onSubmit = e => {
     e.preventDefault();
@@ -47,15 +47,6 @@ const EditProfile = ({
     setTimeout(() => {
       setSuccessMessage("");
     }, 3000);
-
-    setFormData({
-      location: "",
-      age: "",
-      bio: "",
-      twitter: "",
-      facebook: "",
-      instagram: ""
-    });
   };
 
   const onChange = e =>
@@ -64,9 +55,7 @@ const EditProfile = ({
   const { location, age, bio, twitter, facebook, instagram } = formData;
 
   return (
-    <Fragment>
-      <h1 className="large text-primary">Tell us more about yourself</h1>
-
+    <div className="edit-profile-container">
       {/* Success Message Popup */}
       {successMessage && (
         <div className="popup-success">
@@ -74,96 +63,123 @@ const EditProfile = ({
         </div>
       )}
 
-      <form className="form" onSubmit={e => onSubmit(e)}>
-        {/* Your existing fields here */}
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="Age"
-            name="age"
-            value={age}
-            onChange={e => onChange(e)}
-          />
-          <small className="form-text">Age</small>
+      <div className="profile-header">
+        <h1>Edit Your Profile</h1>
+        <p>Let others know more about you</p>
+      </div>
+
+      <form className="profile-form" onSubmit={e => onSubmit(e)}>
+        <div className="form-section">
+          <h2 className="section-title">Personal Information</h2>
+          
+          <div className="form-group">
+            <label htmlFor="age">Age</label>
+            <input
+              type="text"
+              id="age"
+              className="form-control"
+              placeholder="Your age"
+              name="age"
+              value={age}
+              onChange={e => onChange(e)}
+            />
+            <small className="form-text">How old are you?</small>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="location">Location</label>
+            <input
+              type="text"
+              id="location"
+              className="form-control"
+              placeholder="Your location"
+              name="location"
+              value={location}
+              onChange={e => onChange(e)}
+            />
+            <small className="form-text">
+              City & province suggested (eg. Colombo, Western)
+            </small>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="bio">Bio</label>
+            <textarea
+              id="bio"
+              className="form-control textarea-control"
+              placeholder="Tell us about yourself"
+              name="bio"
+              value={bio}
+              onChange={e => onChange(e)}
+            ></textarea>
+            <small className="form-text">Share a brief introduction about yourself</small>
+          </div>
         </div>
 
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="Location"
-            name="location"
-            value={location}
-            onChange={e => onChange(e)}
-          />
-          <small className="form-text">
-            City & province suggested (eg. Colombo, Western)
-          </small>
+        <div className="form-section">
+          <div className="section-title-container">
+            <h2 className="section-title">Social Media Profiles</h2>
+            <button
+              onClick={() => toggleSocialInputs(!displaySocialInputs)}
+              type="button"
+              className="btn btn-social"
+            >
+              <i className={`fas ${displaySocialInputs ? 'fa-minus' : 'fa-plus'}`}></i>
+              {displaySocialInputs ? 'Hide Social Links' : 'Add Social Links'}
+            </button>
+          </div>
+
+          {displaySocialInputs && (
+            <div className="social-inputs">
+              <div className="social-input social-twitter">
+                <i className="fab fa-twitter fa-2x"></i>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Twitter URL"
+                  name="twitter"
+                  value={twitter}
+                  onChange={e => onChange(e)}
+                />
+              </div>
+
+              <div className="social-input social-facebook">
+                <i className="fab fa-facebook fa-2x"></i>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Facebook URL"
+                  name="facebook"
+                  value={facebook}
+                  onChange={e => onChange(e)}
+                />
+              </div>
+
+              <div className="social-input social-instagram">
+                <i className="fab fa-instagram fa-2x"></i>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Instagram URL"
+                  name="instagram"
+                  value={instagram}
+                  onChange={e => onChange(e)}
+                />
+              </div>
+            </div>
+          )}
         </div>
 
-        <div className="form-group">
-          <textarea
-            placeholder="A short bio of yourself"
-            name="bio"
-            value={bio}
-            onChange={e => onChange(e)}
-          ></textarea>
-          <small className="form-text">Tell us a little about yourself</small>
-        </div>
-
-        <div className="my-2">
-          <button
-            onClick={() => toggleSocialInputs(!displaySocialInputs)}
-            type="button"
-            className="btn btn-light"
-          >
-            Add Social Network Links
+        <div className="button-group">
+          <Link className="btn btn-light" to="/dashboard">
+            <i className="fas fa-arrow-left"></i> Cancel
+          </Link>
+          <button type="submit" className="btn btn-primary">
+            <i className="fas fa-save"></i> Save Changes
           </button>
-          <span>Optional</span>
         </div>
-
-        {displaySocialInputs && (
-          <Fragment>
-            <div className="form-group social-input">
-              <i className="fab fa-twitter fa-2x"></i>
-              <input
-                type="text"
-                placeholder="Twitter URL"
-                name="twitter"
-                value={twitter}
-                onChange={e => onChange(e)}
-              />
-            </div>
-
-            <div className="form-group social-input">
-              <i className="fab fa-facebook fa-2x"></i>
-              <input
-                type="text"
-                placeholder="Facebook URL"
-                name="facebook"
-                value={facebook}
-                onChange={e => onChange(e)}
-              />
-            </div>
-
-            <div className="form-group social-input">
-              <i className="fab fa-instagram fa-2x"></i>
-              <input
-                type="text"
-                placeholder="Instagram URL"
-                name="instagram"
-                value={instagram}
-                onChange={e => onChange(e)}
-              />
-            </div>
-          </Fragment>
-        )}
-
-        <input type="submit" className="btn btn-primary my-1" />
-        <Link className="btn btn-light my-1" to="/dashboard">
-          Go Back
-        </Link>
       </form>
-    </Fragment>
+    </div>
   );
 };
 
