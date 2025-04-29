e379ece34ff9a955d13ef718ec42b520f8058dd1
Kavilakshan12 <kavilakshanvijayakumar@gmail.com>
2025-04-29 22:02:36 +0530

    import googleoauthprovider for index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // or 'react-dom' for older versions
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
